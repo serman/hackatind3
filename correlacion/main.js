2c89a28e131ad63284eb11a0705b71f780ceb643
claraizquierdo <claraizquierdosanz@gmail.com>
2018-02-16 21:54:55 +0100

    MVP graph working

--- correlacion/main.js
+++ correlacion/main.js
@@ -1,5 +1,31 @@
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
-    // document ready
+    var vega_config = {
+      "$schema": "https://vega.github.io/schema/vega-lite/v2.json",
+      "data": {"url": "https://raw.githubusercontent.com/serman/hackatind3/master/data/municipalities_data_cluster_y_tasas.csv"},
+      "mark": "point",
+      "encoding": {
+        "x": {"field": "tasa_movilidad","type": "quantitative"},
+        "y": {"field": "renta-bruta-media","type": "quantitative"}, 
+        "color": {"field": "Cluster" }},
+      "width": "500",
+      "height": "500"
+    }
+
+
+    vegaEmbed("#vis", vega_config);
+
+
+    // Event handlers
+    $("[data-name='var-selector']").change(function(e){
+
+      var new_var = e.target.value;
+      vega_config.encoding = {
+        x: {"field": "tasa_movilidad","type": "quantitative"},
+        y: {"field": e.target.value,"type": "quantitative"}
+      }
+
+      vegaEmbed("#vis", vega_config);
+    }); 
   }
 };
